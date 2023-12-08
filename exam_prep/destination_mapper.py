2d4e0e7ac5535bd61b8fd7ec7197803aafc0f32d
AlexandarEfremov <efremov.alex@yahoo.bg>
2023-12-08 07:13:44 +0200

    08/12/23

--- exam_prep/destination_mapper.py
+++ exam_prep/destination_mapper.py
@@ -1,8 +1,21 @@
 import re
-
+total_points = 0
 input_string = input()
+destination_list = []
 pattern = r"=([A-Z][A-Za-z]{2,})=|\/([A-Z][A-Za-z]{2,})\/"
-results = re.findall(pattern, input_string)
-print(results)
-total_points = [len(match) for match in results]
-print(total_points)
+matches = re.finditer(pattern, input_string)
+for match in matches:
+    if match.group(1) is not None:
+        variable_value = match.group(1)
+        variable_len = len(variable_value)
+        total_points += variable_len
+        destination_list.append(variable_value)
+    elif match.group(2) is not None:
+        variable_value = match.group(2)
+        variable_len = len(variable_value)
+        total_points += variable_len
+        destination_list.append(variable_value)
+
+print(f"Destinations: {', '.join(destination_list)}")
+print(f"Travel Points: {total_points}")
+
