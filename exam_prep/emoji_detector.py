edd496d596d5c1f953546bc2b0e57cc927d029ab
AlexandarEfremov <efremov.alex@yahoo.bg>
2023-12-11 06:54:50 +0200

    11/12/23

--- exam_prep/emoji_detector.py
+++ exam_prep/emoji_detector.py
@@ -29,6 +29,3 @@ for emo in emoji_list:
     else:
         continue
 
-
-# In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**
-
