225250110fe7de6299a42f306e0b1fdc2fdbde5b
AlexandarEfremov <efremov.alex@yahoo.bg>
2023-12-11 06:54:09 +0200

    11/12/23

---
+++ exam_prep/emoji_detector.py
@@ -0,0 +1,34 @@
+import re
+raw_input = input()
+pattern = r"((\*\*|::)([A-Z][a-z]{2,})(\2))"
+digit_pattern = r"\d"
+matches = re.finditer(pattern, raw_input)
+all_digits = re.findall(digit_pattern, raw_input)
+
+cool_score = 1
+emoji_list = []
+
+for match in matches:
+    emoji = match.group(1)
+    emoji_list.append(emoji)
+for match in all_digits:
+    cool_score *= int(match)
+
+print(f"Cool threshold: {cool_score}")
+print(f"{len(emoji_list)} emojis found in the text. The cool ones are:")
+for emo in emoji_list:
+    temp_count = 0
+    for letter in emo:
+        if letter == ":" or letter == "*":
+            continue
+        else:
+            ascii_letter = ord(letter)
+            temp_count += ascii_letter
+    if temp_count >= cool_score:
+        print(emo)
+    else:
+        continue
+
+
+# In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**
+
