49a37b4aa58c62db4dda36b11840a9505e96cb8e
Aleksandar Efremov <efremov.alex@yahoo.bg>
2023-12-22 08:11:31 +0200

    22.12.2023

--- dictionaries/moba_challenger.py
+++ dictionaries/moba_challenger.py
@@ -1,19 +1,52 @@
-player_pool = {}
+command_input = input()
 
-while True:
-    input_info = input()
-    if input_info == "Season end":
-        break
-    if "->" in input_info:
-        player, position, skill = input_info.split("->")
-        skill = int(skill)
-        if player not in player_pool:
-            player_pool[player] = []
-            player_pool[player].append({position, skill})
-        else:
-            if player_pool[player][skill] < skill:
-                player_pool[player][skill] = skill
-    elif "vs" in input_info:
-        first_player, second_player = input_info.split("vs")
+player_info = {}
+best_players = []
 
-print(player_pool)
+
+def duel_players(name_one, name_two):
+    if (name_one and name_two) in player_info:
+        players_to_remove = []
+        for p_one in player_info[name_one]:
+            if p_one in player_info[name_two]:
+                total_pl_one = sum(player_info[name_one].values())
+                total_pl_two = sum(player_info[name_two].values())
+                if total_pl_one > total_pl_two:
+                    players_to_remove.append(name_two)
+                elif total_pl_one < total_pl_two:
+                    players_to_remove.append(name_one)
+                break
+
+        for player in players_to_remove:
+            del player_info[player]
+
+
+def adding_players_roles(player, position, skill):
+    if player not in player_info:
+        player_info[player] = {}
+    if position not in player_info[player]:
+        player_info[player][position] = 0
+    if player_info[player][position] < skill:
+        player_info[player][position] = skill
+
+
+while command_input != "Season end":
+    if " vs " in command_input:
+        command_input = command_input.split(" vs ")
+        duel_players(command_input[0], command_input[-1])
+    else:
+        command_input = command_input.split(" -> ")
+        adding_players_roles(command_input[0], command_input[1], int(command_input[-1]))
+    command_input = input()
+
+
+def show_result():
+    for p_name in player_info:
+        best_players.append({"name": p_name, "total_score": sum(player_info[p_name].values())})
+    for show in sorted(best_players, key=lambda item: (-item["total_score"], item["name"])):
+        print(f"{show['name']}: {show['total_score']} skill")
+        for pos, skill in sorted(player_info[show['name']].items(), key=lambda item: (-item[1], item[0])):
+            print(f"- {pos} <::> {skill}")
+
+
+show_result()
